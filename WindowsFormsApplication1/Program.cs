03230b5844b1f76804389974133b2821be66e9f7
danielx <danielx-25@hotmail.com>
2016-02-29 23:08:49 -0300

    inicio

---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.Vista;
+using Npgsql;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Console.WriteLine("hola a todos");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AgregarTesis());
+            BaseDatos basedatos = new BaseDatos();
+            NpgsqlConnection conexion = basedatos.abrirConexion();
+
+            string sql = "INSERT INTO tesis(titulo, estudiantes, profesores, fecha, etiquetas) VALUES('trafico vehicular', 'daniel', 'dytto', '21/02/1992', 'agentes');";
+            string sql1 = "INSERT INTO pimpa VALUES('daniel', 19);";
+
+            //NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conexion); para consultas
+            NpgsqlCommand da = new NpgsqlCommand(sql, conexion);
+            da.ExecuteNonQuery();
+            System.Console.ReadLine();
+
+            //Application.Run(new Form1());
+            //AgregarTesis l = new AgregarTesis();
+        }
+
+
+    }
+}
---
+++ WindowsFormsApplication1/Vista/AgregarTesis.cs
@@ -0,0 +1,55 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.Vista
+{
+    public partial class AgregarTesis : Form
+    {
+        private NpgsqlConnection conexion;
+        private string fila;
+
+        public AgregarTesis()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            agregar_tesis();
+        }
+
+        
+
+        public void setConection(NpgsqlConnection conexion)
+        {
+            this.conexion = conexion;
+        }
+
+        public void agregar_tesis()
+        {
+            if (textBox1.TextLength>1)
+            fila = "'" + textBox1.Text + "', ";
+            else
+            {
+                MessageBox.Show("Campo incompleto", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+                textBox1.SelectionStart = 0;
+            }
+                
+            fila += "'" + textBox2.Text + "', ";
+            fila+= "'" + textBox3.Text + "', ";
+            fila+= "'" + monthCalendar1.SelectionRange.Start.ToShortDateString() + "', ";
+            fila+= "'" + textBox4.Text + "'";
+            System.Console.WriteLine(fila);
+
+        }
+
+    }
+}
